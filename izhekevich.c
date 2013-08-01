bd122edfe70e989fbb4aca7c161c34575cad3946
Michael Schmatz <schmatz@umich.edu>
2013-08-01 14:31:50 -0700

    Random changes on code

--- izhekevich.c
+++ izhekevich.c
@@ -8,21 +8,25 @@
 
 srand(time(NULL)); //seed the random number generator
 
-
+double randomExcitatoryNeurons[NUMBER_EXCITATORY_NEURONS];
 double (*randomExcitatoryNeurons)[NUMBER_EXCITATORY_NEURONS];
 double (*randomInhibitoryNeurons)[NUMBER_INHIBITORY_NEURONS];
 
-double (*amatrix)[NUMBER_EXCITATORY_NEURONS]
+double (*amatrix)[NUMBER_EXCITATORY_NEURONS + NUMBER_INHIBITORY_NEURONS];
+double (*bmatrix)[]
 
-void fillArrayWithRandomNumbers(double *inputMatrix, int arrayLength) {
-	for (int i=0; i < arrayLength; ++i) (*inputMatrix)[i] = (double)rand() / (double)RAND_MAX;
+void fillArrayWithRandomNumbers(double inputArray, int arrayLength) {
+	for (int i=0; i < arrayLength; ++i) inputArray[i] = (double)rand() / (double)RAND_MAX;
 }
 
 //fill the excitatory neurons with random numbers between 0 and 1
-fillArrayWithRandomNumbers(randomExcitatoryNeurons, NUMBER_EXCITATORY_NEURONS);
-fillArrayWithRandomNumbers(randomInhibitoryNeurons, NUMBER_INHIBITORY_NEURONS);
+fillArrayWithRandomNumbers(&randomExcitatoryNeurons, NUMBER_EXCITATORY_NEURONS);
+fillArrayWithRandomNumbers(&randomInhibitoryNeurons, NUMBER_INHIBITORY_NEURONS);
 
 //construct "a" matrix
+
+//take out fired
+//v and u values are relevant
 
 
 int r = rand
