b4cd85edc37c39ba3ff2f675322cda710758ad66
Michael Schmatz <schmatz@umich.edu>
2013-08-11 17:16:44 -0700

    Added original matlab code

--- izhekevich.c
+++ izhekevich.c
@@ -26,13 +26,14 @@ void fillArrayWithRandomNumbers(double *inputArray, int arrayLength) {
 int main(int argc, char **argv)
 {
 	srand(time(NULL)); //seed the random number generator
-	
-	//fill the excitatory neurons with random numbers between 0 and 1
+	//declare two arrays to hold the neurons
 	double randomExcitatoryNeurons[NUMBER_EXCITATORY_NEURONS];
 	double randomInhibitoryNeurons[NUMBER_INHIBITORY_NEURONS];
+	//fill the excitatory neurons with random numbers between 0 and 1
 	fillArrayWithRandomNumbers(randomExcitatoryNeurons, NUMBER_EXCITATORY_NEURONS);
 	fillArrayWithRandomNumbers(randomInhibitoryNeurons, NUMBER_INHIBITORY_NEURONS);
 
+	
 	//use powf(a,b) to do float exponentiation
 
 	//construct "a" matrix
