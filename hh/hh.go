b2a4f62d3168d1ed79553dec1a86b9da3104e684
Michael Schmatz <schmatz@umich.edu>
2013-08-13 14:10:21 -0700

    Started simulation and wrapped at 80 characters

--- hh/hh.go
+++ hh/hh.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"math"
 	//"github.com/sbinet/go-hdf5/pkg/hdf5"
 )
@@ -22,7 +22,8 @@ func potassiumBetaN(voltage float64) float64 {
 
 //calculate potassium steady state activation value
 func potassiumNInfinity(voltage float64) float64 {
-	return potassiumAlphaN(voltage) / (potassiumAlphaN(voltage) + potassiumBetaN(voltage))
+	return potassiumAlphaN(voltage) /
+		(potassiumAlphaN(voltage) + potassiumBetaN(voltage))
 }
 
 //calculate first sodium rate constant
@@ -41,7 +42,8 @@ func sodiumBetaM(voltage float64) float64 {
 
 //calculate sodium steady state activation value
 func sodiumMInfinity(voltage float64) float64 {
-	return sodiumAlphaM(voltage) / (sodiumAlphaM(voltage) + sodiumBetaM(voltage))
+	return sodiumAlphaM(voltage) /
+		(sodiumAlphaM(voltage) + sodiumBetaM(voltage))
 }
 
 //calculate first sodium inactivation rate constant
@@ -56,7 +58,8 @@ func sodiumBetaH(voltage float64) float64 {
 
 //calculate sodium steady state inactivation value
 func sodiumHInfinity(voltage float64) float64 {
-	return sodiumAlphaH(voltage) / (sodiumAlphaH(voltage) + sodiumBetaH(voltage))
+	return sodiumAlphaH(voltage) /
+		(sodiumAlphaH(voltage) + sodiumBetaH(voltage))
 }
 
 func main() {
@@ -66,9 +69,12 @@ func main() {
 	deltaTime := float64(0.025)        //Simulation timestep in milliseconds
 
 	var timeArray []float64 //the array to hold timesteps
-	timeArray = make([]float64, int(totalSimulationTime/deltaTime), int(totalSimulationTime/deltaTime))
-	for timestep := float64(0); timestep < (totalSimulationTime+deltaTime)/deltaTime; timestep++ {
-		timeArray = append(timeArray, timestep*deltaTime) //fill the array with a number of timesteps
+	timeArray = make([]float64, int(totalSimulationTime/deltaTime),
+		int(totalSimulationTime/deltaTime))
+	for timestep := float64(0); timestep <
+		(totalSimulationTime+deltaTime)/deltaTime; timestep++ {
+		//fill the array with a number of timesteps
+		timeArray = append(timeArray, timestep*deltaTime)
 	}
 
 	//Hodgkin Huxley model parameters
@@ -77,24 +83,40 @@ func main() {
 	sodiumActivationMaxConductance := float64(120) //g-_Na
 	potassiumMaxConductance := float64(36)         //g-_K
 	leakConductance := float64(0.3)                //g-_l
-	sodiumVoltagesSource := float64(115)           //E_Na
-	potassiumVoltageSource := float64(-12)         //E_K
-	leakReversePotential := float64(10.613)        //E_l
+	//These values aren't used until the simulation
+	var sodiumConductance, potassiumConductance float64 //g_Na and g_K
+	sodiumVoltagesSource := float64(115)                //E_Na
+	potassiumVoltageSource := float64(-12)              //E_K
+	leakReversePotential := float64(10.613)             //E_l
 	//declare and calculate activation and inactivation dimensionless quantities
 	m := sodiumMInfinity(restVoltage)
 	n := potassiumNInfinity(restVoltage)
 	h := sodiumHInfinity(restVoltage)
 	//make an array of the calculated voltages
-	membraneRestingPotentialDifferentials := make([]float64, len(timeArray), len(timeArray))
+	V_m := make([]float64,
+		len(timeArray), len(timeArray))
 	//set the first timestep equal to the rest voltage
-	membraneRestingPotentialDifferentials[0] = restVoltage
-	//make an an array of stimuli over time, replace this later with other neurons in the network
+	V_m[0] = restVoltage
+	//make an an array of stimuli over time 
+	//replace this later with other neurons in the network
 	stimulusValues := make([]float64, len(timeArray), len(timeArray))
-	fmt.Println("Array length is", len(timeArray))
 	for time, currentTime := range timeArray {
+		//arbitrary stimulation times
 		if currentTime >= 10 && currentTime <= 20 {
-			stimulusValues[time] = float64(20)
+			stimulusValues[time] = float64(20) //some arbitrary stimulus value
 		}
+	}
+	//the main simulation loop
+	for timeStep := range timeArray {
+		if timeStep == 0 {
+			//Skip the very first step of simulation to preserve initial state
+			continue
+		}
+
+		//Run the calculations
+		m = m + (sodiumAlphaM(V_m[timeStep-1])*(1-m)-
+			sodiumBetaM(V_m[timeStep-1])*m)*deltaTime
+
 	}
 
 }
