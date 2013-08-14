9e39bf45a6a04b22df9008f8b1ff27b193c8039c
Michael Schmatz <schmatz@umich.edu>
2013-08-14 13:18:56 -0700

    Progress making neuron class

--- hh/hh.go
+++ hh/hh.go
@@ -6,6 +6,137 @@ import (
 	//"github.com/sbinet/go-hdf5/pkg/hdf5"
 )
 
+type NeuronParameters struct {
+	//these declare constant values
+	restVoltage                    float64 //V_rest
+	lipidBilayerCapacitance        float64 //C_m
+	sodiumActivationMaxConductance float64 //g-_Na
+	potassiumMaxConductance        float64 //g-_K
+	leakConductance                float64 //g-_l
+	sodiumReversePotential         float64 //E_Na
+	potassiumReversePotential      float64 //E_K
+	leakReversePotential           float64 //E_l
+}
+
+func (np NeuronParameters) initializeParametersWithDefaults() {
+	np.restVoltage = 0
+	np.lipidBilayerCapacitance = 1
+	np.sodiumActivationMaxConductance = 120
+	np.potassiumMaxConductance = 36
+	np.leakConductance = 0.3
+	//sodium conductance and potassium conductance are set during the simulation
+	np.sodiumReversePotential = 115
+	np.potassiumReversePotential = -12
+	np.leakReversePotential = 10.613
+}
+
+type Neuron struct {
+	parameters           NeuronParameters
+	sodiumConductance    float64
+	potassiumConductance float64
+	//activation and inactivation dimensionless quantities
+	m               float64
+	n               float64
+	h               float64
+	stimulation     []float64
+	V_m             []float64
+	currentTimeStep float64
+	simulation      *Simulation
+}
+
+func (neuron Neuron) setSimulation(simulation Simulation) {
+	neuron.simulation = &simulation
+}
+
+func (neuron Neuron) setInitialVoltage() {
+	neuron.V_m = append(neuron.V_m, neuron.parameters.restVoltage)
+}
+
+func (neuron Neuron) calculateDimensionlessQuantities() {
+	neuron.m += (sodiumAlphaM(neuron.V_m[int(neuron.currentTimeStep)-1])*(1-neuron.m) -
+		sodiumBetaM(neuron.V_m[int(neuron.currentTimeStep)-1])*neuron.m) * neuron.simulation.deltaTime
+	neuron.h += (sodiumAlphaH(neuron.V_m[timeStep-1])*(1-neuron.h) -
+		sodiumBetaH(neuron.V_m[int(neuron.currentTimeStep)-1])*neuron.h) * neuron.simulation.deltaTime
+	neuron.n += (potassiumAlphaN(neuron.V_m[timeStep-1])*(1-neuron.n) -
+		potassiumBetaN(neuron.V_m[int(neuron.currentTimeStep)-1])*neuron.n) * neuron.simulation.deltaTime
+
+}
+func (neuron Neuron) calculateSimulationStep() {
+	//potassium and sodium calculation step
+	neuron.calculatePotassiumAndSodiumConductance()
+	neuron.calculateNewVoltage()
+
+}
+
+type Simulation struct {
+	totalSimulationTime float64
+	deltaTime           float64
+	timeArray           []float64
+	weightArray         [][]float64
+}
+
+func (n Neuron) calculateNewVoltage() {
+	n.V_m[n.currentTimeStep] = n.V_m[n.currentTimeStep-1]
+	n.V_m[n.currentTimeStep] += (n.stimulation[n.currentTimeStep-1] - n.sodiumConductance*
+		(n.V_m[n.currentTimeStep-1]-n.parameters.sodiumReversePotential) - n.potassiumConductance*
+		(n.V_m[n.currentTimeStep-1]-n.parameters.potassiumReversePotential) - n.parameters.leakConductance*
+		(n.V_m[n.currentTimeStep-1]-n.parameters.leakReversePotential)) / n.parameters.lipidBilayerCapacitance * neuron.simulation.deltaTime
+}
+
+func (neuron Neuron) calculatePotassiumAndSodiumConductance() {
+	neuron.sodiumConductance = neuron.parameters.sodiumActivationMaxConductance * neuron.h * math.Pow(neuron.m, 3.)
+	neuron.potassiumConductance = neuron.parameters.potassiumMaxConductance * math.Pow(neuron.n, 4.)
+}
+
+func (neuron Neuron) currentVoltage() {
+	return neuron.V_m[neuron.currentTimeStep]
+}
+
+func (neuron Neuron) potassiumAlphaN() {
+	if neuron.currentVoltage() != 10 {
+		return 0.01 * (-neuron.currentVoltage() + 10) / (math.Exp((-neuron.currentVoltage()+10)/10) - 1)
+	}
+	return 0.1
+}
+
+func (neuron Neuron) potassiumBetaN() {
+	return 0.125 * math.Exp(-neuron.currentVoltage()/80)
+}
+
+func (neuron Neuron) potassiumNInfinity() {
+	return neuron.potassiumAlphaN() /
+		(neuron.potassiumAlphaN() + neuron.potassiumBetaN())
+}
+
+func (neuron Neuron) sodiumAlphaM() {
+	if neuron.currentVoltage() != 25 {
+		return 0.1 * (-neuron.currentVoltage() + 25) / (math.Exp((-neuron.currentVoltage()+25)/10) - 1)
+	}
+	return 1
+}
+
+func (neuron Neuron) sodiumBetaM() {
+	return 4 * math.Exp(-neuron.currentVoltage()/18)
+}
+
+func (neuron Neuron) sodiumMInfinity() {
+	return neuron.sodiumAlphaM() /
+		(neuron.sodiumAlphaM() + neuron.sodiumBetaM())
+}
+
+func (neuron Neuron) sodiumAlphaH() {
+	return 0.07 * math.Exp(-neuron.currentVoltage()/20)
+}
+
+func (neuron Neuron) sodiumBetaH() {
+	return 1 / (math.Exp((-neuron.currentVoltage()+30)/10) + 1)
+}
+
+func (neuron Neuron) sodiumHInfinity() {
+	return neuron.sodiumAlphaH() /
+		(neuron.sodiumAlphaH() + neuron.sodiumBetaH())
+}
+
 //calculate the potassium rate constant
 func potassiumAlphaN(voltage float64) float64 {
 	if voltage != 10 {
